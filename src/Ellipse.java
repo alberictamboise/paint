25b869df6f79bfd8ff8dd140bc8e362b6fca028f
alberictamboise <alberic.tamboise@gmail.com>
2021-11-25 13:46:43 +0100

    Paint

--- src/Ellipse.java
+++ src/Ellipse.java
@@ -14,33 +14,27 @@ public class Ellipse extends Figure {
     @Override
     public void setBoundingBox(Point x1, Point x2) {
         if (x1.getX() < x2.getX() & x2.getY() > x1.getY()) {
-           // Point newOrigin = new Point(x1.getX(), x1.getY());
-          //  Point endOfLine = new Point(x2.getX(), x2.getY());
             width = (x2.getX() - x1.getX());
             height = (x2.getY() - x1.getY());
             super.x1.x =x1.getX();
             super.x1.y= x1.getY();
         }
         else if (x1.getX() > x2.getX() & x1.getY() < x2.getY()) {
-            //Point newOrigin = new Point(x2.getX(), x1.getY());
-            //Point endOfLine = new Point(x1.getX(), x2.getY());
             super.x1.x =x2.getX();
             super.x1.y= x1.getY();
             width = (x1.getX() - x2.getX());
             height = (x2.getY() - x1.getY());
         }
         else if (x2.getX() < x1.getX() & x2.getY() < x1.getY()) {
-            Point newOrigin = new Point(x2.getX(), x2.getY());
-            Point endOfLine = new Point(x1.getX(), x1.getY());
-            super.x1 = newOrigin;
-            width = (endOfLine.getX() - newOrigin.getX());
-            height = (endOfLine.getY() - newOrigin.getY());
+            super.x1.x =x2.getX();
+            super.x1.y= x2.getY();
+            width = (x1.getX() - x2.getX());
+            height = ( x1.getY() - x2.getY());
         }else if (x1.getX() < x2.getX() & x2.getY() < x1.getY()) {
-            Point newOrigin = new Point(x1.getX(), x2.getY());
-            Point endOfLine = new Point(x2.getX(), x1.getY());
-            super.x1 = newOrigin;
-            width = (endOfLine.getX() - newOrigin.getX());
-            height = (endOfLine.getY() - newOrigin.getY());
+            super.x1.x =x1.getX();
+            super.x1.y= x2.getY();
+            width = (x2.getX() - x1.getX());
+            height = (x1.getY() - x2.getY());
         }
 
     }
