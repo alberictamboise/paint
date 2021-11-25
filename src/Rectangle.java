3a886b7a7bfc7abf83736426092483f87d676239
alberictamboise <alberic.tamboise@gmail.com>
2021-11-25 13:47:59 +0100

    Paint

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -14,49 +14,40 @@ public class Rectangle extends Figure {
         super(c, new Point(px, py));
         setBoundingBox(0, 0);
     }
-
-
-    //problemes d'encapsulation et d'injection de dépandance
     @Override
     public void setBoundingBox(int heigthBB, int widthBB) {
         width = widthBB;
         height = heigthBB;
-
     }
 
     @Override
     public void setBoundingBox(Point x1, Point x2) {
-
-        Point fin = new Point();
-
         if (x1.getX() < x2.getX() & x2.getY() > x1.getY()) {
-            Point newOrigin = new Point(x1.getX(), x1.getY());
-            Point endOfLine = new Point(x2.getX(), x2.getY());
-            height = (endOfLine.getY() - newOrigin.getY());
-            width = (endOfLine.getX() - newOrigin.getX());
+            width = (x2.getX() - x1.getX());
+            height = (x2.getY() - x1.getY());
+            super.x1.x =x1.getX();
+            super.x1.y= x1.getY();
         }
-             else if (x1.getX() > x2.getX() & x1.getY() < x2.getY()) {
-                Point newOrigin = new Point(x2.getX(), x1.getY());
-                Point endOfLine = new Point(x1.getX(), x2.getY());
-                super.x1 = newOrigin;
-                height = (endOfLine.getY() - newOrigin.getY());
-                width = (endOfLine.getX() - newOrigin.getX());
-            }
-         else if (x2.getX() < x1.getX() & x2.getY() < x1.getY()) {
-            Point newOrigin = new Point(x2.getX(), x2.getY());
-            Point endOfLine = new Point(x1.getX(), x1.getY());
-            super.x1 = newOrigin;
-            height = (endOfLine.getY() - newOrigin.getY());
-            width = (endOfLine.getX() - newOrigin.getX());
+        else if (x1.getX() > x2.getX() & x1.getY() < x2.getY()) {
+            super.x1.x =x2.getX();
+            super.x1.y= x1.getY();
+            width = (x1.getX() - x2.getX());
+            height = (x2.getY() - x1.getY());
+        }
+        else if (x2.getX() < x1.getX() & x2.getY() < x1.getY()) {
+            super.x1.x =x2.getX();
+            super.x1.y= x2.getY();
+            width = (x1.getX() - x2.getX());
+            height = ( x1.getY() - x2.getY());
         }else if (x1.getX() < x2.getX() & x2.getY() < x1.getY()) {
-            Point newOrigin = new Point(x1.getX(), x2.getY());
-            Point endOfLine = new Point(x2.getX(), x1.getY());
-            super.x1 = newOrigin;
-            height = (endOfLine.getY() - newOrigin.getY());
-            width = (endOfLine.getX() - newOrigin.getX());
+            super.x1.x =x1.getX();
+            super.x1.y= x2.getY();
+            width = (x2.getX() - x1.getX());
+            height = (x1.getY() - x2.getY());
         }
 
     }
+
     @Override
     public void draw(Graphics g) {
         g.setColor(c);
